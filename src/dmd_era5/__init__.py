0c710b6dd5ef33857fd6b44d5a3fad1ced916576
Lydia France <lfrance@turing.ac.uk>
2024-10-17 22:02:41 +0100

    Fix init file

--- src/dmd_era5/__init__.py
+++ src/dmd_era5/__init__.py
@@ -8,7 +8,15 @@ from importlib.metadata import version
 
 from .config_reader import config_reader
 from .logger import setup_logger, log_and_print
+from .era5_download import config_parser, download_era5_data, create_mock_era5
 
 
-__all__ = ["config_reader", "setup_logger", "log_and_print"]
+__all__ = [
+    "config_reader",
+    "setup_logger",
+    "log_and_print",
+    "config_parser",
+    "download_era5_data",
+    "create_mock_era5",
+]
 __version__ = version(__name__)
