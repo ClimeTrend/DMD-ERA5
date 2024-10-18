bbe22baf8eebe5640d4a4830cd5709acb9892c8f
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-10-18 17:05:10 +0100

    update subpackage __init__

--- src/dmd_era5/era5_download/__init__.py
+++ src/dmd_era5/era5_download/__init__.py
@@ -1,4 +1,15 @@
-from dmd_era5.era5_download.era5_download import config_parser, download_era5_data
 from dmd_era5.era5_download.create_mock_era5 import create_mock_era5
+from dmd_era5.era5_download.era5_download import (
+    config_parser,
+    download_era5_data,
+    slice_era5_dataset,
+    thin_era5_dataset,
+)
 
-__all__ = ["config_parser", "download_era5_data", "create_mock_era5"]
+__all__ = [
+    "config_parser",
+    "download_era5_data",
+    "create_mock_era5",
+    "slice_era5_dataset",
+    "thin_era5_dataset",
+]
