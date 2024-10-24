6c9b910ff1da189c7ee06767a6340448f8a72424
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-10-24 10:37:53 +0100

    test for missing field in config parser

--- tests/era5_svd/test_era5_svd.py
+++ tests/era5_svd/test_era5_svd.py
@@ -17,9 +17,32 @@ def base_config():
         "svd_type": "randomized",
         "delay_embedding": 2,
         "standardize": False,
+        "start_datetime": "2019-01-01T00",
+        "end_datetime": "2020-01-01T00",
     }
 
 
-def test_config_parser():
-    config = config_parser()
-    assert isinstance(config, dict)
+def test_config_parser(base_config):
+    parsed_config = config_parser(base_config)
+    assert isinstance(parsed_config, dict)
+
+
+@pytest.mark.parametrize(
+    "field",
+    [
+        "file_path",
+        "save_name",
+        "variables",
+        "levels",
+        "svd_type",
+        "delay_embedding",
+        "standardize",
+        "start_datetime",
+        "end_datetime",
+    ],
+)
+def test_config_parser_missing_field(base_config, field):
+    """Test the missing field error."""
+    del base_config[field]
+    with pytest.raises(ValueError, match=f"Missing required field in config: {field}"):
+        config_parser(base_config)
