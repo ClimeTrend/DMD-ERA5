7ed84ee45701d4a77cc99a3ea0609f7f42c5eb20
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-10-24 09:22:31 +0100

    define base config pytest fixture

--- tests/era5_svd/test_era5_svd.py
+++ tests/era5_svd/test_era5_svd.py
@@ -2,7 +2,22 @@
 Tests for the era5_svd module.
 """
 
+import pytest
+
 from dmd_era5.era5_svd import config_parser
+
+
+@pytest.fixture
+def base_config():
+    return {
+        "file_path": "",
+        "save_name": "",
+        "variables": "temperature",
+        "levels": "all",
+        "svd_type": "randomized",
+        "delay_embedding": 2,
+        "standardize": False,
+    }
 
 
 def test_config_parser():
