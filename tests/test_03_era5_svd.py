53cfbdaa776565f2e4addb9113581f8625c252c1
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-10-28 14:49:47 +0000

    remove unnecessary test

--- tests/test_03_era5_svd.py
+++ tests/test_03_era5_svd.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 import pytest
 
-from dmd_era5 import create_mock_era5
 from dmd_era5.era5_svd import config_parser
 
 
@@ -71,15 +70,3 @@ def test_config_parser_invalid_delay_embedding(base_config):
     base_config["delay_embedding"] = 0
     with pytest.raises(ValueError, match="Invalid delay embedding in config"):
         config_parser(base_config)
-
-
-@pytest.mark.skip("work in progress")
-def test_config_parser_invalid_start_datetime(base_config):
-    """Test that a requested start datetime is invalid when it is out of range."""
-    mock_era5 = create_mock_era5(
-        "2020-01-01T00",
-        "2020-01-05T00",
-        variables=["temperature"],
-        levels=[1000],
-    )
-    pass
