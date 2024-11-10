1f7669376bff8c2bc678f0e702a1fc7c8ee76f97
LydiaFrance <lfrance@turing.ac.uk>
2024-11-10 17:15:31 +0000

    Fix circular import

--- src/dmd_era5/era5_processing/__init__.py
+++ src/dmd_era5/era5_processing/__init__.py
@@ -1,4 +1,4 @@
-from dmd_era5.era5_processing import slice_era5_dataset, thin_era5_dataset
+from dmd_era5.era5_processing.era5_processing import slice_era5_dataset, thin_era5_dataset
 
 __all__ = [
     "slice_era5_dataset",
