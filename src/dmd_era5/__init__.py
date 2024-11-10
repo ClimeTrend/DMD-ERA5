cf73820c6d97cc0e6b20f940b56c98fc0c0df454
LydiaFrance <lfrance@turing.ac.uk>
2024-11-10 18:07:10 +0000

    Precommit hook: reorder imports

--- src/dmd_era5/__init__.py
+++ src/dmd_era5/__init__.py
@@ -7,9 +7,9 @@ from __future__ import annotations
 from importlib.metadata import version
 
 from dmd_era5.config_reader import config_reader
-from dmd_era5.logger import log_and_print, setup_logger
 from dmd_era5.create_mock_data import create_mock_era5
-from dmd_era5.slice_tools import slice_era5_dataset, thin_era5_dataset, standardize_data
+from dmd_era5.logger import log_and_print, setup_logger
+from dmd_era5.slice_tools import slice_era5_dataset, standardize_data, thin_era5_dataset
 
 __all__ = [
     "config_reader",
--- src/dmd_era5/slice_tools/__init__.py
+++ src/dmd_era5/slice_tools/__init__.py
@@ -1,7 +1,7 @@
 from dmd_era5.slice_tools.slice_tools import (
     slice_era5_dataset,
-    thin_era5_dataset,
     standardize_data,
+    thin_era5_dataset,
 )
 
 __all__ = [
