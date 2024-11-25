3842926505ed8446314ac110dd2150d2017ac07a
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-11-25 14:06:33 +0000

    Update era5_svd tests to include 'delta_time' field and validate 'era5-svd' section in config_parser

--- tests/test_03_era5_svd.py
+++ tests/test_03_era5_svd.py
@@ -2,12 +2,13 @@
 Tests for the era5_svd module.
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
+from dmd_era5 import config_parser
 from dmd_era5.create_mock_data import create_mock_era5
-from dmd_era5.era5_svd import config_parser, svd_on_era5
+from dmd_era5.era5_svd import svd_on_era5
 
 
 @pytest.fixture
@@ -15,18 +16,19 @@ def base_config():
     return {
         "source_path": "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2",
         "variables": "temperature",
-        "levels": "all",
+        "levels": "1000",
         "svd_type": "randomized",
         "delay_embedding": 2,
         "mean_center": False,
         "scale": False,
         "start_datetime": "2019-01-01T06",
         "end_datetime": "2020-01-01T12",
+        "delta_time": "1h",
     }
 
 
-def test_config_parser(base_config):
-    parsed_config = config_parser(base_config)
+def test_config_parser_basic(base_config):
+    parsed_config = config_parser(base_config, section="era5-svd")
     assert isinstance(parsed_config, dict)
     assert parsed_config["start_datetime"] == datetime(
         2019, 1, 1, 6, 0
@@ -36,11 +38,13 @@ def test_config_parser(base_config):
         2020, 1, 1, 12, 0
     ), f"""end_datetime should be {datetime(2020, 1, 1, 12, 0)}
     not {parsed_config['end_datetime']}"""
+    assert parsed_config["delta_time"] == timedelta(hours=1)
 
 
 @pytest.mark.parametrize(
     "field",
     [
+        "source_path",
         "variables",
         "levels",
         "svd_type",
@@ -49,27 +53,28 @@ def test_config_parser(base_config):
         "scale",
         "start_datetime",
         "end_datetime",
+        "delta_time",
     ],
 )
 def test_config_parser_missing_field(base_config, field):
     """Test the missing field error in the configuration."""
     del base_config[field]
     with pytest.raises(ValueError, match=f"Missing required field in config: {field}"):
-        config_parser(base_config)
+        config_parser(base_config, section="era5-svd")
 
 
 def test_config_parser_invalid_svd_type(base_config):
     """Test invalid SVD type in the configuration."""
     base_config["svd_type"] = "invalid"
     with pytest.raises(ValueError, match="Invalid SVD type in config"):
-        config_parser(base_config)
+        config_parser(base_config, section="era5-svd")
 
 
 def test_config_parser_invalid_delay_embedding(base_config):
     """Test invalid delay embedding in the configuration."""
     base_config["delay_embedding"] = 0
     with pytest.raises(ValueError, match="Invalid delay embedding in config"):
-        config_parser(base_config)
+        config_parser(base_config, section="era5-svd")
 
 
 def test_svd_on_era5(base_config):
@@ -79,5 +84,5 @@ def test_svd_on_era5(base_config):
         variables=["temperature"],
         levels=[1000],
     )
-    parsed_config = config_parser(base_config)
+    parsed_config = config_parser(base_config, section="era5-svd")
     svd_on_era5(parsed_config, mock_era5)
