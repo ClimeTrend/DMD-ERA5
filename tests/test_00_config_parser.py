19f7e2403476410eca8b662ed869f7f9876b719c
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-11-25 16:22:28 +0000

    parametrize de config_parser tests for era5-download and era5-svd

--- tests/test_00_config_parser.py
+++ tests/test_00_config_parser.py
@@ -10,7 +10,7 @@ from dmd_era5 import config_parser
 
 
 @pytest.fixture
-def base_config():
+def base_config_era5_download():
     return {
         "source_path": "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2",
         "start_datetime": "2019-01-01T00",
@@ -18,11 +18,25 @@ def base_config():
         "delta_time": "1y",
         "variables": "all",
         "levels": "1000",
-        "save_name": "",
     }
 
 
-# Test datetime fields
+@pytest.fixture
+def base_config_era5_svd():
+    return {
+        "source_path": "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2",
+        "start_datetime": "2019-01-01T00",
+        "end_datetime": "2020-01-01T00",
+        "delta_time": "1y",
+        "variables": "all",
+        "levels": "1000",
+        "svd_type": "randomized",
+        "delay_embedding": 2,
+        "mean_center": True,
+        "scale": True,
+    }
+
+
 @pytest.mark.parametrize(
     ("datetime_field", "invalid_datetime"),
     [
@@ -31,14 +45,24 @@ def base_config():
         ("end_datetime", "2019-01-01T25"),
     ],
 )
-def test_config_parser_invalid_datetime(base_config, datetime_field, invalid_datetime):
+@pytest.mark.parametrize("section", ["era5-download", "era5-svd"])
+def test_config_parser_invalid_datetime(
+    base_config_era5_download,
+    base_config_era5_svd,
+    datetime_field,
+    invalid_datetime,
+    section,
+):
     """Test the invalid datetime error."""
-    base_config[datetime_field] = invalid_datetime
+    if section == "era5-download":
+        config = base_config_era5_download
+    elif section == "era5-svd":
+        config = base_config_era5_svd
+    config[datetime_field] = invalid_datetime
     with pytest.raises(ValueError, match="Invalid datetime"):
-        config_parser(base_config, section="era5-download")
+        config_parser(config, section=section)
 
 
-# Test delta_time field
 @pytest.mark.parametrize(
     ("delta_time", "expected"),
     [
@@ -48,18 +72,39 @@ def test_config_parser_invalid_datetime(base_config, datetime_field, invalid_dat
         ("7d", timedelta(days=7)),
     ],
 )
-def test_config_parser_delta_time(base_config, delta_time, expected):
+@pytest.mark.parametrize("section", ["era5-download", "era5-svd"])
+def test_config_parser_delta_time(
+    base_config_era5_download,
+    base_config_era5_svd,
+    delta_time,
+    expected,
+    section,
+):
     """Test the delta_time field."""
-    base_config["delta_time"] = delta_time
-    parsed_config = config_parser(base_config, section="era5-download")
+    if section == "era5-download":
+        config = base_config_era5_download
+    elif section == "era5-svd":
+        config = base_config_era5_svd
+    config["delta_time"] = delta_time
+    parsed_config = config_parser(config, section=section)
     assert (
         parsed_config["delta_time"] == expected
     ), f"Expected delta_time to be {expected}, but got {parsed_config['delta_time']}"
 
 
 @pytest.mark.parametrize("invalid_delta", ["1v", "1", "not-a-delta"])
-def test_config_parser_invalid_delta_time(base_config, invalid_delta):
+@pytest.mark.parametrize("section", ["era5-download", "era5-svd"])
+def test_config_parser_invalid_delta_time(
+    base_config_era5_download,
+    base_config_era5_svd,
+    invalid_delta,
+    section,
+):
     """Test the invalid delta_time error."""
-    base_config["delta_time"] = invalid_delta
+    if section == "era5-download":
+        config = base_config_era5_download
+    elif section == "era5-svd":
+        config = base_config_era5_svd
+    config["delta_time"] = invalid_delta
     with pytest.raises(ValueError, match="Error parsing delta_time"):
-        config_parser(base_config, section="era5-download")
+        config_parser(config, section=section)
