dff28b1153b43103593cdc6c291b98eeead54e98
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-11-29 22:27:51 +0000

    update tests following constraints on ERA5 variables to download

--- tests/test_03_era5_download.py
+++ tests/test_03_era5_download.py
@@ -8,6 +8,7 @@ import pytest
 import xarray as xr
 
 from dmd_era5 import config_parser
+from dmd_era5.constants import ERA5_PRESSURE_LEVEL_VARIABLES
 from dmd_era5.era5_download import download_era5_data
 
 
@@ -18,7 +19,7 @@ def base_config():
         "start_datetime": "2019-01-01T00",
         "end_datetime": "2020-01-01T00",
         "delta_time": "1y",
-        "variables": "all",
+        "variables": "all_pressure_level_vars",
         "levels": "1000",
     }
 
@@ -41,9 +42,10 @@ def test_config_parser_basic(base_config):
     assert parsed_config["delta_time"] == timedelta(
         days=365
     ), f"delta_time should be {timedelta(hours=1)} not {parsed_config['delta_time']}"
-    assert parsed_config["variables"] == [
-        "all"
-    ], f"variables should be ['all'] not {parsed_config['variables']}"
+    assert parsed_config["variables"] == list(ERA5_PRESSURE_LEVEL_VARIABLES), f"""
+    variables should be {list(ERA5_PRESSURE_LEVEL_VARIABLES)}
+    not {parsed_config['variables']}
+    """
     assert parsed_config["levels"] == [
         1000
     ], f"levels should be [1000] not {parsed_config['levels']}"
@@ -97,9 +99,12 @@ def test_config_parser_levels(base_config, levels, expected):
 @pytest.mark.parametrize(
     ("variables", "expected"),
     [
-        ("temperature,humidity,pressure", ["temperature", "humidity", "pressure"]),
-        ("all", ["all"]),
-        (" temperature , humidity ", ["temperature", "humidity"]),
+        (
+            "temperature,u_component_of_wind,v_component_of_wind",
+            ["temperature", "u_component_of_wind", "v_component_of_wind"],
+        ),
+        ("all_pressure_level_vars", list(ERA5_PRESSURE_LEVEL_VARIABLES)),
+        (" temperature , u_component_of_wind ", ["temperature", "u_component_of_wind"]),
     ],
 )
 def test_config_parser_variables(base_config, variables, expected):
