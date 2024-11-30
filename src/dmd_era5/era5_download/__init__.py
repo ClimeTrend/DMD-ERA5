38f8e969684951a4196708de3087b2ec7961cc42
Lydia France <lfrance@turing.ac.uk>
2024-11-30 20:40:40 +0000

    Remove circular imports, only download fcns

--- src/dmd_era5/era5_download/__init__.py
+++ src/dmd_era5/era5_download/__init__.py
@@ -1,15 +1,8 @@
 from dmd_era5.era5_download.era5_download import (
     add_config_attributes,
-    add_data_to_dvc,
-    config_parser,
     download_era5_data,
-    main,
+    main
 )
 
-__all__ = [
-    "config_parser",
-    "download_era5_data",
-    "add_data_to_dvc",
-    "add_config_attributes",
-    "main",
-]
+
+__all__ = ["download_era5_data", "add_config_attributes", "main"]
