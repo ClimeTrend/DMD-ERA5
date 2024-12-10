6adefac4c81251bc875b798451fe02c2d5fc9f57
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-12-10 18:40:21 +0000

    add retrieve_era5_slice to __init__.py and update __all__ exports

--- src/dmd_era5/era5_svd/__init__.py
+++ src/dmd_era5/era5_svd/__init__.py
@@ -1,3 +1,7 @@
-from dmd_era5.era5_svd.era5_svd import combine_svd_results, svd_on_era5
+from dmd_era5.era5_svd.era5_svd import (
+    combine_svd_results,
+    retrieve_era5_slice,
+    svd_on_era5,
+)
 
-__all__ = ["svd_on_era5", "combine_svd_results"]
+__all__ = ["svd_on_era5", "combine_svd_results", "retrieve_era5_slice"]
