fe79a86f48e1a4fac245a6f620ed856a7773da8b
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-12-11 20:46:47 +0000

    add retrieve_svd_results to module exports

--- src/dmd_era5/era5_svd/__init__.py
+++ src/dmd_era5/era5_svd/__init__.py
@@ -1,7 +1,13 @@
 from dmd_era5.era5_svd.era5_svd import (
     combine_svd_results,
     retrieve_era5_slice,
+    retrieve_svd_results,
     svd_on_era5,
 )
 
-__all__ = ["svd_on_era5", "combine_svd_results", "retrieve_era5_slice"]
+__all__ = [
+    "svd_on_era5",
+    "combine_svd_results",
+    "retrieve_era5_slice",
+    "retrieve_svd_results",
+]
