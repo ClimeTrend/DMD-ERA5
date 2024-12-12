b8d426fdb77cd1d5833f029a64087a066cd5c182
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-12-12 11:29:47 +0000

    add tests for DVC file and log creation, including metadata validation

--- tests/test_05_dvc_era5_svd.py
+++ tests/test_05_dvc_era5_svd.py
@@ -1,4 +1,5 @@
 import pytest
+import yaml
 from dvc.repo import Repo as DvcRepo
 from git import Repo as GitRepo
 from pyprojroot import here
@@ -11,13 +12,14 @@ from dmd_era5.era5_svd import (
     retrieve_era5_slice,
 )
 
+dvc_file_path = "data/era5_svd/2019-01-01T00_2019-01-01T04_1h.nc.dvc"
+dvc_log_path = "data/era5_svd/2019-01-01T00_2019-01-01T04_1h.nc.yaml"
+data_path = "data/era5_svd/2019-01-01T00_2019-01-01T04_1h.nc"
+
 
 @pytest.mark.dependency(name="test_retrieve_era5_slice")
 @pytest.mark.docker
-@pytest.mark.parametrize(
-    "config",
-    ["era5_svd_config_a", "era5_svd_config_b", "era5_svd_config_c"],
-)
+@pytest.mark.parametrize("config", ["era5_svd_config_a", "era5_svd_config_b"])
 def test_retrieve_era5_slice(config, request):
     """Test the retrieve_era5_slice function using DVC."""
     config_dict = request.getfixturevalue(config)
@@ -40,7 +42,9 @@ def test_retrieve_era5_slice(config, request):
 
 @pytest.mark.dependency(name="test_add_era5_svd")
 @pytest.mark.docker
-@pytest.mark.parametrize("config", ["era5_svd_config_a", "era5_svd_config_b"])
+@pytest.mark.parametrize(
+    "config", ["era5_svd_config_a", "era5_svd_config_b", "era5_svd_config_c"]
+)
 def test_add_era5_svd_to_dvc(config, request):
     """
     Test that SVD results can be added to and tracked by
@@ -68,3 +72,41 @@ def test_add_era5_svd_to_dvc(config, request):
         repo.index.commit("Add SVD results to DVC")
     with DvcRepo(here()) as repo:
         repo.push()  # push to the remote DVC repository
+
+
+@pytest.mark.dependency(name="test_dvc_file_and_log", depends=["test_add_era5_svd"])
+@pytest.mark.docker
+def test_dvc_file_and_log():
+    """
+    Test that the DVC file and log are correctly created, are
+    tracked by Git, and contain the expected metadata.
+    """
+
+    # check that the DVC file and log have been created and committed
+    with GitRepo(here()) as repo:
+        dvc_file = list(repo.iter_commits(all=True, max_count=10, paths=dvc_file_path))
+        dvc_log = list(repo.iter_commits(all=True, max_count=10, paths=dvc_log_path))
+    assert len(dvc_file) == 3, "There should be 3 commits for the DVC file"
+    assert len(dvc_log) == 3, "There should be 3 commits for the DVC log file"
+
+    # check that the log file contains the expected metadata
+    with open(dvc_log_path) as f:
+        dvc_log_content = yaml.safe_load(f)
+    assert len(dvc_log_content) == 3, "The log file should contain 3 entries"
+    dvc_log_keys = list(dvc_log_content.keys())
+    assert dvc_log_content[dvc_log_keys[0]]["variables"] == [
+        "temperature"
+    ], "The first entry of the log should contain temperature data"
+    assert bool(dvc_log_content[dvc_log_keys[0]]["scale"]) is False, """
+    The first entry of the log should not have been scaled
+    """
+    assert dvc_log_content[dvc_log_keys[1]]["variables"] == [
+        "u_component_of_wind"
+    ], "The second entry of the log should contain u-wind data"
+    assert dvc_log_content[dvc_log_keys[2]]["variables"] == [
+        "temperature",
+        "v_component_of_wind",
+    ], "The third entry of the log should contain temperature and v-wind data"
+    assert bool(dvc_log_content[dvc_log_keys[2]]["scale"]) is True, """
+    The third entry of the log should have been scaled
+    """
