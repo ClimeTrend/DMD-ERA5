0cfd8ea97a6faeddc2f486c718a018167bd2b83a
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-12-16 23:12:40 +0000

    add era5_svd main to init file

--- src/dmd_era5/era5_svd/__init__.py
+++ src/dmd_era5/era5_svd/__init__.py
@@ -1,6 +1,7 @@
 from dmd_era5.era5_svd.era5_svd import (
     add_config_attributes,
     combine_svd_results,
+    main,
     retrieve_era5_slice,
     retrieve_svd_results,
     svd_on_era5,
@@ -12,4 +13,5 @@ __all__ = [
     "retrieve_era5_slice",
     "retrieve_svd_results",
     "add_config_attributes",
+    "main",
 ]
