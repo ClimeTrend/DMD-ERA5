4bc1963821a9b950989d63f666eb2b71eae31954
Lydia France <lfrance@turing.ac.uk>
2025-01-24 10:19:04 +0000

    Change imports

--- src/dmd_era5/core.py
+++
@@ -1,7 +0,0 @@
-"""Core functionality shared across modules"""
-
-from dmd_era5.config_parser import config_parser
-from dmd_era5.config_reader import config_reader
-from dmd_era5.logger import log_and_print, setup_logger
-
-__all__ = ["config_parser", "config_reader", "log_and_print", "setup_logger"]
